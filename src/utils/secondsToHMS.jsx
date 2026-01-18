683bf4dacf35dcfa6ac20905f1e033688a03084a
Dan Nguyen <dnguye97@asu.edu>
2026-01-17 18:53:35 -0700

    added possible todo change

--- src/utils/secondsToHMS.jsx
+++ src/utils/secondsToHMS.jsx
@@ -19,7 +19,7 @@ export function secondsToHMSTuple(seconds) {
 	seconds = Number(seconds);
 	const hours = seconds > 3600 ? Math.floor(seconds / 3600) : null;
 	const minutes = hours ? Math.floor((seconds % 3600) / 60) : Math.floor(seconds / 60);
-	const remainingSeconds = Math.round(seconds % 60);
+	const remainingSeconds = Math.round(seconds % 60);		// TODO change to floor?
 
 	return {
 		hours: hours,
